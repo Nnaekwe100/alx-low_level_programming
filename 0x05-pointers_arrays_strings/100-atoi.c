e8a389a1faf0f9ffe8ec20906a786a1cfd001369
Nnaekwehumphrey100 <nnaekwehumphrey@gmail.com>
2023-09-19 15:45:54 -0700

    working

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -32,11 +32,11 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[x + 1] < '0' || s[x + 1] > '9')
 				break;
 			f = 0;
 		}
-		i++;
+		x++;
 	}
 	if (f == 0)
 		return (0);
